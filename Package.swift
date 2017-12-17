8dd8125de43c6c7baa24d738625a0123daee5989
Dan Lindsay <dlindsay72@hotmail.com>
2017-12-17 15:36:14 -0500

    merged get-post, added new dependency to Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url:"https://github.com/matthijs2704/vapor-apns.git", majorVersion: 2, minor: 0)
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
